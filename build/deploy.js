7e58d3edd57a1a2f6101fc97f84a003f18e737ac
Pig Fang <g-plane@hotmail.com>
2018-05-01 19:54:31 +0800

    use circleci

---
+++ build/deploy.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const ghpages = require('gh-pages')
+
+fs.mkdirSync('./dist/.circleci')
+fs.copyFileSync('./static/.circleci/config.yml', './dist/.circleci/config.yml')
+
+ghpages.publish('dist', {
+  user: {
+    name: 'GPlane Fans',
+    email: 'gplane@protonmail.com'
+  },
+  dotfiles: true,
+  // eslint-disable-next-line max-len
+  repo: `https://${process.env.GITHUB_TOKEN}@github.com/g-plane/eslint-playground.git`
+}, err => {
+  if (err) {
+    process.stderr.write(err)
+    process.exitCode = 1
+  } else {
+    process.exitCode = 0
+  }
+})
