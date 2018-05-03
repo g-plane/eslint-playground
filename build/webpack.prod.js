740220108c332b69ffa7ea1c279a70c469d9665b
Pig Fang <g-plane@hotmail.com>
2018-05-04 00:27:07 +0800

    fix build

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,22 +1,26 @@
 const path = require('path')
+const webpack = require('webpack')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const merge = require('webpack-merge')
 const base = require('./webpack.config')
 
 module.exports = merge(base, {
-  mode: 'production',
+  mode: 'none',
   output: {
     filename: '[name].[chunkhash].js',
     chunkFilename: '[name].[chunkhash].js',
     path: path.resolve(__dirname, '..', 'dist'),
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    }),
     new UglifyJSPlugin({
       parallel: true,
       uglifyOptions: {
         ecma: 6,
-        compress: {},
+        compress: false,
         mangle: false
       }
     }),
