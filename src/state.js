e97c692f975b404c65c0dcb14e78eb711274871c
Pig Fang <g-plane@hotmail.com>
2021-03-21 11:01:45 +0800

    clean up repository

--- src/state.js
+++
@@ -1,9 +0,0 @@
-import pako from 'pako'
-
-export function serialize(state) {
-  return btoa(pako.deflate(JSON.stringify(state), { to: 'string' }))
-}
-
-export function deserialize(string) {
-  return JSON.parse(pako.inflate(atob(string), { to: 'string' }))
-}
