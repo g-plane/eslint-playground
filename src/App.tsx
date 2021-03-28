3e7e87da4b3686250f5e4a8035ac7c7168bc5833
Pig Fang <g-plane@hotmail.com>
2021-03-28 19:36:23 +0800

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import Header from './components/Header'
 import Workspace from './components/Workspace'
 
+const theme = extendTheme({
+  colors: {
+    brand: '#4B32C3',
+  },
+})
+
 const App: React.FC = () => {
   return (
-    <>
+    <ChakraProvider theme={theme}>
       <Header />
       <Workspace />
-    </>
+    </ChakraProvider>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,11 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import './index.css'
 import App from './App'
 
-const theme = extendTheme({
-  colors: {
-    brand: '#4B32C3',
-  },
-})
-
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 )
