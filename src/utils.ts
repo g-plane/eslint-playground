36c2d1ce2bbebf803cb177f4d98a6a5af905abc6
Pig Fang <g-plane@hotmail.com>
2021-04-01 12:02:33 +0800

    tweak

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@ import type * as monaco from 'monaco-editor'
 export const defaultMonacoOptions: monaco.editor.IStandaloneEditorConstructionOptions = {
   fontSize: 15,
   lineHeight: 24,
-  fontFamily: '"Cascadia Code", "JetBrains Mono", Monaco, Consolas, monospace',
+  fontFamily: '"JetBrains Mono", monospace',
 }
 
 export const defaultEditorConfig: monaco.editor.IEditorOptions &
